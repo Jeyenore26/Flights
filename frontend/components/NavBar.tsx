34588cd1ed9423bed49af80b659981fd674aeb76
Jeyenore <78587875+Jeyenore26@users.noreply.github.com>
2022-11-28 14:14:12 +0200

    dead

---
+++ frontend/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <ul>
+      <li> home </li>
+      <li> 5altk </li>
+      <li> mama </li>
+      <li> papa </li>
+    </ul>
+  )
+}
