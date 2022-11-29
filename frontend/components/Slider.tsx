12b6d8ccbf553c5d30181b64211aaeb2b9002d0c
TruAmakku <iiherogamer18@gmail.com>
2022-11-29 07:21:04 +0200

    Update Slider.tsx

--- frontend/components/Slider.tsx
+++ frontend/components/Slider.tsx
@@ -1,27 +1,35 @@
-import React, { useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper";
+import SwiperCore, { Autoplay } from "swiper";
 
-export default function App() {
+export default () => {
+  SwiperCore.use([Autoplay]);
   return (
-    <>
-      <Swiper
-        navigation={true}
-        modules={[Navigation]}
-        className="h-[400px] w-[400px]"
-      >
-        <SwiperSlide className="slides">Slide 1</SwiperSlide>
-        <SwiperSlide className="slides">Slide 2</SwiperSlide>
-        <SwiperSlide className="slides">Slide 3</SwiperSlide>
-        <SwiperSlide className="slides">Slide 4</SwiperSlide>
-        <SwiperSlide className="slides">Slide 5</SwiperSlide>
-        <SwiperSlide className="slides">Slide 6</SwiperSlide>
-        <SwiperSlide className="slides">Slide 7</SwiperSlide>
-        <SwiperSlide className="slides">Slide 8</SwiperSlide>
-        <SwiperSlide className="slides">Slide 9</SwiperSlide>
-      </Swiper>
-    </>
+    <Swiper
+      slidesPerView={1}
+      autoplay={{
+        delay: 10000,
+      }}
+      loop={true}
+    >
+      <SwiperSlide className="slides">
+        <div className="w-full flex">
+          <img
+            className="h-[400px] w-full opacity-[0.9] "
+            src="https://images.hdqwalls.com/wallpapers/eiffel-tower-paris-4k-wide.jpg"
+            alt="paris"
+          ></img>
+          <div className="absolute bg-black m-4 top-[80px] p-5 w-[30%]">
+            <h1>Paris</h1>
+            <h3>Book N'Fly</h3>
+            <br />
+            <p>pretty nice gaauy croasont Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide className="slides">Slide 2</SwiperSlide>
+      <SwiperSlide className="slides">Slide 3</SwiperSlide>
+      <SwiperSlide className="slides">Slide 4</SwiperSlide>
+    </Swiper>
   );
-}
+};
