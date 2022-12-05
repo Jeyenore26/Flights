4a0071a9bfd11cc52efc90e99d1c0e7412c45367
TruAmakku <iiherogamer18@gmail.com>
2022-12-05 22:20:09 +0200

    Update EarthImg.tsx
    s

--- frontend/components/EarthImg.tsx
+++ frontend/components/EarthImg.tsx
@@ -5,11 +5,11 @@ function EarthImg() {
   return (
     <div>
       <ParallaxProvider>
-        <Parallax speed={-10}>
+        <Parallax speed={-20}>
           <img
             src="./earthing.jpg"
             alt="s"
-            className="absolute mt-[-7px] z-[-1] h-[100vh] top-[-5.8rem] brightness-50"
+            className="absolute mt-[-7px] z-[-1] h-[100vh] top-[-12rem] brightness-50"
           />
         </Parallax>
       </ParallaxProvider>
