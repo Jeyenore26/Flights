52b9a1f9aa52fdffdbb77328fc21a24af44caa24
Jeyenore <78587875+Jeyenore26@users.noreply.github.com>
2022-12-09 19:06:55 +0200

    dead

---
+++ frontend/components/MainNav.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
+import { ImEarth } from "react-icons/im";
+import wallpaper from "../public/earthwallpaper.png";
+export default function MainNav() {
+  return (
+    <div>
+      <div className="py-4 flex items-center justify-between relative z-[1] ">
+          
+      </div>
+    </div>
+  );
+}
---
+++ frontend/pages/mainpage.tsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+ import MainNav from '../components/MainNav'
+ export default function mainpage() {
+   return (
+    <>
+        <section className='w-[100%] h-screen bg-indigo-900 '>
+            <div className='2xl:mx-[4rem] xl:mx-[4rem] lg:mx-[4rem] md:mx-[4rem] sm:mx-[1rem] xs:mx-[0rem] xxs:mx-[0rem]'>
+            <MainNav/>
+            </div>
+        </section>
+    </>
+   )
+ }
+ 
