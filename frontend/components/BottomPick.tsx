3a81c1a81e715a5ffd68034d3c2d1352e276feab
Jeyenore <78587875+Jeyenore26@users.noreply.github.com>
2022-12-09 23:23:13 +0200

    suiiii

---
+++ frontend/components/BottomPick.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import  {HiOutlineMagnifyingGlassCircle , HiOutlineMagnifyingGlassMinus} from 'react-icons/hi2'  
+import {IoWaterOutline} from 'react-icons/io5'
+import {CgTrash} from 'react-icons/cg'
+import {GiPineTree , GiBlackFlag} from 'react-icons/gi'
+export default function BottomPick() {
+  const [on , seton] = useState(false)
+    return (
+    <div className=' fixed bottom-0 right-0 p-3 m-4  '>
+        {!on && (
+           <div className="bg-gray-800 rounded-full p-4">
+             <span className="text-white bg-gray-800 rounded-full hover:bg-gray-600 active:bg-gray-700 hover:text-white/70 active:text-white/90">
+                <HiOutlineMagnifyingGlassCircle className="w-8 h-8" onClick={()=>{
+                    seton(!on)
+                }}/>
+            </span>
+           </div>
+        )}
+        {on && (
+            <>
+             <div className="my-5 m-3 border-[2px] border-full rounded-full p-1 hover:text-black/50 hover:border-black/40 active:text-black/90 active:border-black/80 border-gray-700">
+                <IoWaterOutline className="w-7 h-7"/>
+            </div>
+            <div className="my-5 m-3 flex justify-center border-[2px] border-full rounded-full p-1 hover:text-black/20 hover:border-black/40 active:text-black/90 active:border-black/80 border-gray-700">
+                <GiBlackFlag className="w-7 h-7 "/>
+            </div>
+            <div className="my-5 m-3 border-[2px] border-full rounded-full p-1 hover:text-black/20 hover:border-black/40 active:text-black/90 active:border-black/80 border-gray-700">
+                <GiPineTree className="w-7 h-7 "/>
+            </div>
+            <div className="my-5 m-3 border-[2px] border-full rounded-full p-1 hover:text-black/20 hover:border-black/40 active:text-black/90 active:border-black/80 border-gray-700">
+                <CgTrash className="w-7 h-7 "/>
+            </div>
+            <div className="bg-gray-800 rounded-full p-4">
+                    
+             <span className="text-white bg-gray-800 rounded-full flex justify-center hover:bg-gray-600 active:bg-gray-700 hover:text-white/70 active:text-white/90">
+                <HiOutlineMagnifyingGlassMinus className="w-8 h-8" onClick={()=>{
+                    seton(!on)
+                }}/>
+            </span>
+           </div>
+          
+            </>
+        )}
+    </div>
+  )
+}
--- frontend/pages/mainpage.tsx
+++ frontend/pages/mainpage.tsx
@@ -1,12 +1,12 @@
  import React from 'react'
+import BottomPick from '../components/BottomPick'
  import MainNav from '../components/MainNav'
  export default function mainpage() {
    return (
     <>
-        <section className='w-[100%] h-screen bg-indigo-900 '>
-            <div className='2xl:mx-[4rem] xl:mx-[4rem] lg:mx-[4rem] md:mx-[4rem] sm:mx-[1rem] xs:mx-[0rem] xxs:mx-[0rem]'>
+        <section className='w-[100%] h-screen bg-slate-200 '>
             <MainNav/>
-            </div>
+            <BottomPick/>
         </section>
     </>
    )
