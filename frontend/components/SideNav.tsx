935e81a4ec71132e8bd8321068d53251b72d77f1
Jeyenore <78587875+Jeyenore26@users.noreply.github.com>
2022-12-10 13:32:19 +0200

    ddd

--- frontend/components/SideNav.tsx
+++ frontend/components/SideNav.tsx
@@ -9,7 +9,7 @@ import {AiFillSetting} from 'react-icons/ai'
 import wallpaper from "../public/LogoIcon.png";
 export default function SideNav() {
   return (
-    <div className="rounded-l-md bg-gray-800 h-full w-[13rem] flex   sidenavhidden  z-0 fixed right-0 ">
+    <div className="rounded-l-md bg-gray-800 h-full w-[13rem] flex overflow-auto  sidenavhidden  z-0 absolute right-0 ">
         <div className="z-50 text-white mt-4 space-y-12 mr-2 ">
         <div>
           <span className="flex justify-center">
@@ -47,12 +47,6 @@ export default function SideNav() {
 
             <h1 className="mx-3 mt-1 cairo_semibold_title text-xl">ساعد البيئة</h1>
           </button>
-          <button className="flex justify-end w-full py-1 mx-auto mt-12 hover:bg-slate-500 rounded-lg border-white sidenav focus:bg-white/20 focus:rounded-r-none focus:border-r-[2px] ">
-          <FaTrashRestore className="mx-2 w-8 h-8"/>
-
-            <h1 className="mx-3 mt-1 cairo_semibold_title text-xl">اعادة التدوير</h1>
-          </button>
-          
             </div>
         </div>
         
@@ -61,12 +55,12 @@ export default function SideNav() {
             <p></p>
             <p></p>
           </div>
-        <button className="flex absolute bottom-20 justify-end w-full py-1 mx-auto mt-12 hover:bg-slate-500 rounded-lg border-white sidenav focus:bg-white/20 focus:rounded-r-none focus:border-r-[2px] ">
+        <button className="flex justify-end w-full py-1 mx-auto mt-12 hover:bg-slate-500 rounded-lg border-white sidenav focus:bg-white/20 focus:rounded-r-none focus:border-r-[2px] ">
           <AiFillSetting className="mx-2 w-8 h-8"/>
 
             <h1 className="mx-3 mt-1 cairo_semibold_title text-lg">ألاعادادت</h1>
           </button>
-        <button className="flex absolute bottom-5 justify-end w-full py-1 mx-auto mt-12 hover:bg-slate-500 rounded-lg border-white sidenav focus:bg-white/20 focus:rounded-r-none focus:border-r-[2px] ">
+        <button className="flex justify-end w-full py-1 mx-auto mt-12 hover:bg-slate-500 rounded-lg border-white sidenav focus:bg-white/20 focus:rounded-r-none focus:border-r-[2px] ">
           <GiExitDoor className="mx-2 w-8 h-8"/>
 
             <h1 className="mx-3 mt-1 cairo_semibold_title text-lg">تسجيل الخروج</h1>
