d3170a9febdf90d99ebf4736d65b1c42f084a90b
Jeyenore <78587875+Jeyenore26@users.noreply.github.com>
2022-12-13 21:57:09 +0200

    d

---
+++ frontend/pages/creategroup.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function creategroup() {
+  return (
+    <>
+    <div className='bg-gradient-to-b from-[#000000] to-[#00186d] h-screen w-full'     
+>
+        <section  className=" 2xl:mx-[8rem] xl:mx-[8rem] lg:mx-[7rem] md:mx-[6rem]  border-[#7e868a] border-[3px] rounded-2xl h-3/4  ">
+
+        </section>
+    </div>
+    </>
+  )
+}
