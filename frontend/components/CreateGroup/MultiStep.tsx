beffe339b165b4ac2018b85466d71fa7810db847
Jeyenore <jeyenorbro118@gmail.com>
2022-12-27 16:59:10 -0800

    wdadawdawdawdawdaw

--- frontend/components/CreateGroup/MultiStep.tsx
+++ frontend/components/CreateGroup/MultiStep.tsx
@@ -14,6 +14,14 @@ function getToken() {
 export default function () {
   /* page one*/
   const [ grouppic , setgrouppic] = useState('')
+  const [ provepic1 , setprovepic1] = useState('')
+  const [ provepic2 , setprovepic2] = useState('')
+  const [ provepic3 , setprovepic3] = useState('')
+  const [ provepic4 , setprovepic4] = useState('')
+  const [ provepic5 , setprovepic5] = useState('')
+  const [ provepic6 , setprovepic6] = useState('')
+
+
   const [groupname, setgroupname] = useState("");
   const [groupnumber, setgroupnumber] = useState("");
   const [groupprovide, setgroupprovide] = useState("");
@@ -49,26 +57,28 @@ export default function () {
   if (data) console.log(data);
   function pageone() {
     event?.preventDefault();
-    console.log(typeof groupnumber);
     setpage(2);
   }
   function pagetwo() {
     event?.preventDefault();
-    groupInput();
+    setpage(page+1);
   }
+    //groupInput();
 
   function pagethree() {
     event?.preventDefault();
+    setpage(page+1);
+
   }
   const [page, setpage] = useState(1);
   const [disable, setdis] = useState(false);
-  useEffect(() => {
-    if (page == 3) {
-      setdis(true);
-    } else {
-      setdis(false);
-    }
-  });
+  //useEffect(() => {
+  //  if (page == 3) {
+ //     setdis(true);
+  //  } else {
+  //    setdis(false);
+  //  }
+ // });
   const pagecolor = { color: "blue" };
   return (
     <>
@@ -78,7 +88,7 @@ export default function () {
       >
         <div className="flex-1 h-full mt-20 max-w-5xl mx-auto bg-white rounded-lg shadow-xl">
           <div className="flex flex-col md:flex-row">
-            <div className="h-32 xxs:h-10 xxs:mt-0 md:mt-8  object-cover md:h-auto md:w-1/2">
+            <div className="h-32 xxs:h-10 xxs:mt-0 md:mt-0  object-cover md:h-auto md:w-1/2">
               <img
                 className="object-cover xxs:h-0  md:block w-full md:h-full brightness-90"
                 src="./group1.jpg"
@@ -114,6 +124,15 @@ export default function () {
                     }
                   >
                     الخطوة الثانية
+                  </a>
+                  <a
+                    className={
+                      page === 3
+                        ? "pageon sm:px-6 sm:w-auto sm:justify-start cairo_regular_title"
+                        : "pageoff sm:px-6 sm:w-auto sm:justify-start hover:text-gray-900 cairo_regular_title"
+                    }
+                  >
+                    الخطوة الاخيرة
                   </a>
                 </div>
                 {/* First Page*/}
@@ -181,11 +200,11 @@ export default function () {
                 </form>
                 {/* Second Page*/}
 
-                {page === 2 && (
+                {page === 2&& (
                   <>
                     <form
                       onSubmit={pagetwo}
-                      style={{ width: page === 2 ? "100%" : "35%" }}
+                      style={{ width: page === 2? "100%" : "35%" }}
                       className="PageContent"
                     >
                       <div className="PageContent">
@@ -208,14 +227,55 @@ export default function () {
                           lclassName="text-black"
                         />
                       </div>
-                      <div className="PageContent">
-                      <ImageInput required  value ={grouppic} onChange={(e) => {setgrouppic(e.target.value)}} label="صورة البوست" lclassName="text-black text-start  cairo_regular_title" type={'file'} />
+                      <div className="PageContent rounded-xl px-[1px]  py-[10px] text-[16px] text-[#2c3e50]">
+                      <ImageInput required image={grouppic}  value ={grouppic} onChange={(e) => {setgrouppic(e.target.value)}} label="صورة البوست" lclassName="text-black text-start cairo_regular_title" type={'text'} />
 
                       </div>
                       <div className="flex justify-start">
                         <button
                           type="submit"
                           style={{ display: page === 2 ? "block" : "none" }}
+                          className={
+                            disable
+                              ? "px-6 py-2 mt-4 text-sm font-medium leading-5 text-center text-black transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg focus:outline-none cairo_regular_title"
+                              : "px-6 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-[#39A059] hover:bg-[#277941] active:bg-[#235e35] border border-transparent rounded-lg focus:outline-none cairo_regular_title"
+                          }
+                        >
+                          الخطوة القادمة
+                        </button>
+                      </div>
+                    </form>
+                  </>
+                )}
+                {/*Last Page */}
+                {page === 3 && (
+                  <>
+                    <form
+                      onSubmit={pagethree}
+                      style={{ width: page === 3 ? "100%" : "35%" }}
+                      className="PageContent"
+                    >
+                      <div className="flex justify-start text-[1rem] mt-5 cairo_bold_title text-black ">شهادات</div>
+                      <div className="PageContent flex rounded-xl px-[1px]  py-[10px] text-[16px] text-[#2c3e50] ">
+                      <ImageInput required image={provepic1}  value ={provepic1} onChange={(e) => {setprovepic1(e.target.value)}} label="(اجباري) " iclassName="w-[90%]" lclassName="text-black text-center cairo_regular_title text-[14px]"  type={'text'} />
+                      <ImageInput required image={provepic2}  value ={provepic2} onChange={(e) => {setprovepic2(e.target.value)}} label="(اجباري) " lclassName="text-black text-center cairo_regular_title text-[14px]"  type={'text'} />
+
+                      </div>
+                      <div className="PageContent flex rounded-xl px-[1px]  py-[10px] text-[16px] text-[#2c3e50]">
+                      <ImageInput required image={provepic3}   value ={provepic3} onChange={(e) => {setprovepic3(e.target.value)}} label="(اجباري) " iclassName="w-[90%]" lclassName="text-black text-center cairo_regular_title text-[14px]"  type={'text'} />
+                      <ImageInput required image={provepic4}  value ={provepic4} onChange={(e) => {setprovepic4(e.target.value)}} label="(اجباري) " lclassName="text-black text-center cairo_regular_title text-[14px]"  type={'text'} />
+
+                      </div>
+                      <div className="PageContent flex rounded-xl px-[1px]  py-[10px] text-[16px] text-[#2c3e50]">
+                      <ImageInput  image={provepic5}  value ={provepic5} onChange={(e) => {setprovepic5(e.target.value)}} label="(اختياري) " iclassName="w-[90%]" lclassName="text-black text-center cairo_regular_title text-[14px]"  type={'text'} />
+                      <ImageInput  image={provepic6}  value ={provepic6} onChange={(e) => {setprovepic6(e.target.value)}} label="(اختياري) " lclassName="text-black text-center cairo_regular_title text-[14px]"  type={'text'} />
+
+                      </div>
+                      
+                      <div className="flex justify-start">
+                        <button
+                          type="submit"
+                          style={{ display: page === 3 ? "block" : "none" }}
                           className={
                             disable
                               ? "px-6 py-2 mt-4 text-sm font-medium leading-5 text-center text-black transition-colors duration-150 bg-gray-300 border border-transparent rounded-lg focus:outline-none cairo_regular_title"
